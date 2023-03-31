ea8f6c2c7b6f48f31e6f25f04593f18ded80d8a0
Mahfuz <aamahfuz.pro@gmail.com>
2023-04-01 00:01:15 +0600

    first commit

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import "./Header.css";
+
 const Header = () => {
     return (
         <div>
-        <nav className='header'>
+        <nav className='header col-md-12'>
         <div><h3>Programming Boss</h3></div>
-        <div className='item'>
+        {/* <div className='item'>
         <a href="/Shop">Shop</a>
         <a href="/Order">Order</a>
         <a href="/Login">Login</a>
         <a href="/Inventory">Inventory</a>
-        </div>
+        </div> */}
+        <img src="https://i.postimg.cc/vHWLbGnN/Whats-App-Image-2023-03-22-at-7-47-32-PM.jpg" alt="" />
     </nav>
         </div>
     );
